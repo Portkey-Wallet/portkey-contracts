91339291350efbb8f6cfd2cf7dc3f8eb6565341d
nill66 <sofiat.nill@portkey.finance>
2024-02-03 18:21:53 +0800

    feat: ut

--- test/Portkey.Contracts.CA.Tests/CAContractTests_Strategy.cs
+++ test/Portkey.Contracts.CA.Tests/CAContractTests_Strategy.cs
@@ -1,4 +1,8 @@
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Threading.Tasks;
+using Google.Protobuf;
+using Google.Protobuf.Collections;
 using Shouldly;
 using Xunit;
 
@@ -28,4 +32,89 @@ public partial class CAContractTests
         output.Output.BoolResult.ShouldBeTrue();
         //output.Output.StrategyOutput.Name.ShouldBeEquivalentTo(StrategyName.IfElse);
     }
+
+    [Fact]
+    public async Task StrategyHelper_Test()
+    {
+        var notStrategy = new CAContract.NotStrategy();
+        notStrategy.Parse(CAContract.Strategy.DefaultStrategy());
+
+        var addStrategy = new CAContract.AddStrategy
+        {
+            One = new CAContract.NotLargerThanStrategy()
+            {
+                One = CAContractConstants.GuardianCount,
+                Two = 3
+            },
+            Two = new CAContract.NotLessThanStrategy()
+            {
+                One = CAContractConstants.GuardianApprovedCount,
+                Two = 2
+            }
+        };
+        var node = addStrategy.ToStrategyNode();
+        var strategy = CAContract.StrategyFactory.Create(node);
+        bool res = (bool)strategy.Validate(new CAContract.StrategyContext()
+        {
+            Variables = new Dictionary<string, long>
+            {
+                {"guardianCount", 3},
+                {"guardianApprovedCount", 3}
+            }
+        });
+        res.ShouldBeTrue();
+        
+        res = (bool)strategy.Validate(new CAContract.StrategyContext()
+        {
+            Variables = new Dictionary<string, long>
+            {
+                {"guardianCount", 3},
+                {"guardianApprovedCount", 1}
+            }
+        });
+        res.ShouldBeFalse();
+        
+        var orStrategy = new CAContract.OrStrategy()
+        {
+            One = new CAContract.NotLargerThanStrategy()
+            {
+                One = CAContractConstants.GuardianCount,
+                Two = 3
+            },
+            Two = new CAContract.NotLessThanStrategy()
+            {
+                One = CAContractConstants.GuardianApprovedCount,
+                Two = 3
+            }
+        };
+        node = orStrategy.ToStrategyNode();
+        strategy = CAContract.StrategyFactory.Create(node);
+        res = (bool)strategy.Validate(new CAContract.StrategyContext()
+        {
+            Variables = new Dictionary<string, long>
+            {
+                {"guardianCount", 4},
+                {"guardianApprovedCount", 1}
+            }
+        });
+        res.ShouldBeFalse();
+        res = (bool)strategy.Validate(new CAContract.StrategyContext()
+        {
+            Variables = new Dictionary<string, long>
+            {
+                {"guardianCount", 4},
+                {"guardianApprovedCount", 3}
+            }
+        });
+        res.ShouldBeTrue();
+        
+    }
+
+    private async Task ValidateStrategyShouldBe(bool expected, ValidateStrategyInput input)
+    {
+        var output = await CaContractStub.ValidateStrategy.SendAsync(input);
+        output.Output.BoolResult.ShouldBe(expected);
+    }
+    
+    
 }
