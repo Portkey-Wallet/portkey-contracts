e74e0911590dbeb1be5cc288599822e84bb93bc3
steven <steven@portkey.finance>
2024-09-13 14:00:45 +0800

    chore: Remove unneeded ZkTreeVerifier

--- contract/ZkTreeVerifier/ZkWasmVerifierState.cs
+++
@@ -1,9 +0,0 @@
-using AElf.Sdk.CSharp.State;
-
-namespace AElf.Contracts.ZkTreeVerifier
-{
-    // The state class is access the blockchain state
-    public class ZkTreeVerifierState : ContractState 
-    {
-    }
-}
